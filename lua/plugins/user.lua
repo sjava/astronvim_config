62aa7c630e08d880d1e030186efada777bcd3793
sjava <515957966@qq.com>
2024-06-13 15:47:22 +0800

    feat: add nvim-cmp-rust plugin with custom sorting

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -543,8 +543,37 @@ return {
       })
     end,
   },
-  { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
-  -- {
+  {
+    "ryo33/nvim-cmp-rust",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      local cmp = require "cmp"
+      local compare = require "cmp.config.compare"
+      cmp.setup.filetype({ "rust" }, {
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            -- deprioritize `.box`, `.mut`, etc.
+            require("cmp-rust").deprioritize_postfix,
+            -- deprioritize `Borrow::borrow` and `BorrowMut::borrow_mut`
+            require("cmp-rust").deprioritize_borrow,
+            -- deprioritize `Deref::deref` and `DerefMut::deref_mut`
+            require("cmp-rust").deprioritize_deref,
+            -- deprioritize `Into::into`, `Clone::clone`, etc.
+            require("cmp-rust").deprioritize_common_traits,
+            compare.offset,
+            compare.exact,
+            compare.score,
+            compare.recently_used,
+            compare.locality,
+            compare.sort_text,
+            compare.length,
+            compare.order,
+          },
+        },
+      })
+    end,
+  }, -- {
   --   "elixir-tools/elixir-tools.nvim",
   --   ft = { "elixir" },
   --   init = function()

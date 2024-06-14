7b2bcdbb79e8761a922c6f50441189a040b3a523
sjava <515957966@qq.com>
2024-06-14 16:24:51 +0800

    feat(user-plugins): remove nvim-spider plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -213,16 +213,6 @@ return {
     opts = { use_winbar = "smart", show_prompt = false },
   },
   {
-    "chrisgrieser/nvim-spider",
-    event = "User AstroFile",
-    config = function()
-      require("spider").setup { skipInsignificantPunctuation = false }
-      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-    end,
-  },
-  {
     "briangwaltney/paren-hint.nvim",
     event = "User AstroFile",
     dependencies = {

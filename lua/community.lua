56351b65e9db17977aa089872d158c17e2edc6fe
sjava <515957966@qq.com>
2024-06-25 17:00:50 +0800

    feat(user.lua): add grug-far and toggleterm plugins

--- lua/community.lua
+++ lua/community.lua
@@ -122,7 +122,6 @@ return {
     opts = { custom_textobjects = { t = false }, n_lines = 500 },
   },
 
-  { import = "astrocommunity.project.nvim-spectre" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },

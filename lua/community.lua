2c8a8c9281a549839e7ace4b71efe459e6c56e77
sjava <515957966@qq.com>
2024-07-11 09:25:13 +0800

    and its auto_open feature were previously disabled. This commit enables both features to improve the development experience.

--- lua/community.lua
+++ lua/community.lua
@@ -88,9 +88,9 @@ return {
             ["cmp.entry.get_documentation"] = true,
           },
           signature = {
-            enabled = false,
+            enabled = true,
             auto_open = {
-              enabled = false,
+              enabled = true,
               trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
               luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
               throttle = 50, -- Debounce lsp signature help request by 50ms

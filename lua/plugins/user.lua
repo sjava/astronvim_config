10e5ee5aa24cb2e55163355d1c13c6a265e721de
sjava <515957966@qq.com>
2024-08-14 09:26:14 +0800

    feat(plugins): replace markdown preview plugins
    - Removed `iamcco/markdown-preview.nvim` and `MeanderingProgrammer/markdown.nvim`
    - Added `OXY2DEV/markview.nvim` for improved markdown rendering

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -128,22 +128,9 @@ return {
     config = function() require("numb").setup() end,
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
-  },
-  {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
     opts = {},
-  },
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    config = function() require("render-markdown").setup {} end,
   },
   {
     "TwIStOy/luasnip-snippets",
@@ -192,6 +179,18 @@ return {
     "jakewvincent/mkdnflow.nvim",
     ft = "markdown",
     config = function() require("mkdnflow").setup {} end,
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
   {
     "pechorin/any-jump.vim",

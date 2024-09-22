099458765ede7da8c70235c79787958afcaddfc3
sjava <zhu.yuanbin@gmail.com>
2024-09-22 16:02:04 +0800

    remove zen-mode.nvim's open and close hook

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -251,13 +251,6 @@ return {
     end,
   },
   {
-    "folke/zen-mode.nvim",
-    opts = {
-      on_open = function() vim.cmd "TWEnable" end,
-      on_close = function() vim.cmd "TWDisable" end,
-    },
-  },
-  {
     "ysmb-wtsg/in-and-out.nvim",
     keys = {
       {

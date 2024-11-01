026015541e7c3ebe6ac53e59de639d987f1007f0
sjava <zhu.yuanbin@gmail.com>
2024-11-01 22:40:38 +0800

    chore: remove tiny-code-action.nvim plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -335,21 +335,4 @@ return {
       }
     end,
   },
-  {
-    "rachartier/tiny-code-action.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    event = "LspAttach",
-    config = function()
-      require("tiny-code-action").setup()
-      vim.keymap.set(
-        "n",
-        "<leader>lb",
-        function() require("tiny-code-action").code_action() end,
-        { noremap = true, silent = true, desc = "Code Action List" }
-      )
-    end,
-  },
 }

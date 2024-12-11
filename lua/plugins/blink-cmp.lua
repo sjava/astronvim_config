a7a99dfe5ebba8086716cd0f9c1099f7217c47c3
sjava <515957966@qq.com>
2024-12-11 09:21:19 +0800

    fix(blink): sources config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,64 +1,17 @@
-local function has_words_before()
-  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
 return {
   {
     "Saghen/blink.cmp",
     event = "InsertEnter",
     build = "cargo build --release",
     dependencies = { "rafamadriz/friendly-snippets" },
-    opts_extend = { "sources.completion.enabled_providers" },
+    opts_extend = { "sources.default" },
     opts = {
       -- remember to enable your providers here
       sources = {
-        completion = {
-          enabled_providers = {
-            "lsp",
-            "path",
-            "snippets",
-            "buffer",
-          },
-        },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
       -- experimental auto-brackets support
       accept = { auto_brackets = { enabled = false } },
-      -- keymap = {
-      --   ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-      --   ["<Up>"] = { "select_prev", "fallback" },
-      --   ["<Down>"] = { "select_next", "fallback" },
-      --   ["<C-N>"] = { "select_next", "show" },
-      --   ["<C-P>"] = { "select_prev", "show" },
-      --   ["<C-J>"] = { "select_next", "fallback" },
-      --   ["<C-K>"] = { "select_prev", "fallback" },
-      --   ["<C-U>"] = { "scroll_documentation_up", "fallback" },
-      --   ["<C-D>"] = { "scroll_documentation_down", "fallback" },
-      --   ["<C-e>"] = { "hide", "fallback" },
-      --   ["<CR>"] = { "accept", "fallback" },
-      --   ["<Tab>"] = {
-      --     function(cmp)
-      --       if cmp.windows.autocomplete.win:is_open() then
-      --         return cmp.select_next()
-      --       elseif cmp.is_in_snippet() then
-      --         return cmp.snippet_forward()
-      --       elseif has_words_before() then
-      --         return cmp.show()
-      --       end
-      --     end,
-      --     "fallback",
-      --   },
-      --   ["<S-Tab>"] = {
-      --     function(cmp)
-      --       if cmp.windows.autocomplete.win:is_open() then
-      --         return cmp.select_prev()
-      --       elseif cmp.is_in_snippet() then
-      --         return cmp.snippet_backward()
-      --       end
-      --     end,
-      --     "fallback",
-      --   },
-      -- },
       trigger = {
         signature_help = {
           enabled = true,
@@ -93,11 +46,10 @@ return {
               if pcall(require, "lazydev.integrations.blink") then
                 return require("astrocore").extend_tbl(opts, {
                   sources = {
-                    -- add lazydev to your completion providers
-                    completion = { enabled_providers = { "lazydev" } },
+                    default = { "lazydev" },
                     providers = {
                       -- dont show LuaLS require statements when lazydev has items
-                      lsp = { fallback_for = { "lazydev" } },
+                      lsp = { fallbacks = { "lazydev" } },
                       lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
                     },
                   },

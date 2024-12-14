1355ada7f59729add31844e5745584a6debe40db
sjava <zhu.yuanbin@gmail.com>
2024-12-14 17:58:55 +0800

    disable petertriho/cmp-git

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -54,6 +54,7 @@ return {
       { "hrsh7th/nvim-cmp", enabled = false },
       { "rcarriga/cmp-dap", enabled = false },
       { "L3MON4D3/LuaSnip", enabled = false },
+      { "petertriho/cmp-git", enabled = false },
       { "onsails/lspkind.nvim", enabled = false },
     },
   },

010048b27c957b81047afcaf270e1a1254caa4be
sjava <515957966@qq.com>
2024-12-19 09:26:53 +0800

    refactor blink.nvim config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,10 +1,15 @@
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 return {
   {
     "Saghen/blink.cmp",
     event = "InsertEnter",
     build = "cargo build --release",
     dependencies = { "rafamadriz/friendly-snippets" },
-    opts_extend = { "sources.default" },
+    opts_extend = { "sources.default", "sources.cmdline" },
     opts = {
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
@@ -16,44 +21,70 @@ return {
           winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
         },
       },
+      -- keymap = {
+      --   ["<Tab>"] = { "select_next", "fallback" }, -- snippets
+      --   ["<S-Tab>"] = { "select_prev", "fallback" },
+      --   ["<C-y>"] = { "accept", "fallback" },
+      --   ["<C-e>"] = { "show", "hide", "fallback" },
+      --   ["<C-_>"] = { "cancel", "fallback" },
+      --   ["<C-Space>"] = { "show_documentation", "hide_documentation", "fallback" },
+      --   ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      --   ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      -- },
       keymap = {
-        ["<Tab>"] = { "select_next", "fallback" }, -- snippets
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<C-y>"] = { "accept", "fallback" },
-        ["<C-e>"] = { "show", "hide", "fallback" },
-        ["<C-_>"] = { "cancel", "fallback" },
-        ["<C-Space>"] = { "show_documentation", "hide_documentation", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-N>"] = { "select_next", "show" },
+        ["<C-P>"] = { "select_prev", "show" },
+        ["<C-J>"] = { "select_next", "fallback" },
+        ["<C-K>"] = { "select_prev", "fallback" },
+        ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.is_visible() then
+              return cmp.select_next()
+            elseif cmp.snippet_active { direction = 1 } then
+              return cmp.snippet_forward()
+            elseif has_words_before() then
+              return cmp.show()
+            end
+          end,
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          function(cmp)
+            if cmp.is_visible() then
+              return cmp.select_prev()
+            elseif cmp.snippet_active { direction = -1 } then
+              return cmp.snippet_backward()
+            end
+          end,
+          "fallback",
+        },
       },
       completion = {
-        list = { selection = "auto_insert" },
-        accept = { auto_brackets = { enabled = false } },
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 0,
+          auto_show_delay_ms = 200,
           window = {
             border = "rounded",
             winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
           },
         },
         menu = {
-          draw = { columns = { { "label", "label_description", gap = 1 }, { "kind" } } },
+          border = "rounded",
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
         },
       },
-      -- completion = {
-      --   documentation = {
-      --     auto_show = true,
-      --     window = {
-      --       border = "rounded",
-      --       winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-      --     },
-      --   },
-      -- },
     },
     specs = {
       {
         "folke/lazydev.nvim",
+        optional = true,
         specs = {
           {
             "Saghen/blink.cmp",
@@ -61,11 +92,10 @@ return {
               if pcall(require, "lazydev.integrations.blink") then
                 return require("astrocore").extend_tbl(opts, {
                   sources = {
+                    -- add lazydev to your completion providers
                     default = { "lazydev" },
                     providers = {
-                      -- dont show LuaLS require statements when lazydev has items
-                      lsp = { fallbacks = { "lazydev" } },
-                      lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+                      lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
                     },
                   },
                 })

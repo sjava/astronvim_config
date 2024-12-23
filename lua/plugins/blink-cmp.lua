e6e21ac7670665199b89fa1c78d968dc7ce3b505
sjava <zhu.yuanbin@gmail.com>
2024-12-23 23:43:44 +0800

    set blink-cmp to use mini.icons

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,37 +3,6 @@ local function has_words_before()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
-local function get_icon_provider()
-  local _, mini_icons = pcall(require, "mini.icons")
-  if _G.MiniIcons then
-    return function(kind) return mini_icons.get("lsp", kind or "") end
-  end
-  local lspkind_avail, lspkind = pcall(require, "lspkind")
-  if lspkind_avail then
-    return function(kind) return lspkind.symbolic(kind, { mode = "symbol" }) end
-  end
-end
----@type function|false|nil
-local icon_provider = false
-
-local function get_icon(ctx)
-  ctx.kind_hl_group = "BlinkCmpKind" .. ctx.kind
-  if ctx.item.source_name == "LSP" then
-    -- TODO: uncomment after nvim-highlight-colors PR merged: https://github.com/brenoprata10/nvim-highlight-colors/pull/135
-    -- local highlight_colors_avail, highlight_colors = pcall(require, "nvim-highlight-colors")
-    -- local color_item = highlight_colors_avail and highlight_colors.format(ctx.item.documentation, { kind = ctx.kind })
-    if icon_provider == false then icon_provider = get_icon_provider() end
-    if icon_provider then
-      local icon = icon_provider(ctx.kind)
-      if icon then ctx.kind_icon = icon end
-    end
-    -- if color_item and color_item.abbr and color_item.abbr_hl_group then
-    --   ctx.kind_icon, ctx.kind_hl_group = color_item.abbr, color_item.abbr_hl_group
-    -- end
-  end
-  return ctx
-end
-
 return {
   {
     "Saghen/blink.cmp",
@@ -52,16 +21,6 @@ return {
           winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
         },
       },
-      -- keymap = {
-      --   ["<Tab>"] = { "select_next", "fallback" }, -- snippets
-      --   ["<S-Tab>"] = { "select_prev", "fallback" },
-      --   ["<C-y>"] = { "accept", "fallback" },
-      --   ["<C-e>"] = { "show", "hide", "fallback" },
-      --   ["<C-_>"] = { "cancel", "fallback" },
-      --   ["<C-Space>"] = { "show_documentation", "hide_documentation", "fallback" },
-      --   ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      --   ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      -- },
       keymap = {
         ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
         ["<Up>"] = { "select_prev", "fallback" },
@@ -112,11 +71,16 @@ return {
           draw = {
             components = {
               kind_icon = {
+                ellipsis = false,
                 text = function(ctx)
-                  get_icon(ctx)
-                  return ctx.kind_icon .. ctx.icon_gap
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
                 end,
-                highlight = function(ctx) return get_icon(ctx).kind_hl_group end,
+                -- Optionally, you may also use the highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
               },
             },
           },

6aaf4bad7de3a96f7e9c224f6b67a4eb6fbed2ef
sjava <zhu.yuanbin@gmail.com>
2025-03-04 22:33:53 +0800

    fix: copilotchat-nvim default model

--- lua/community.lua
+++ lua/community.lua
@@ -104,13 +104,12 @@ return {
   { import = "astrocommunity.editing-support.treesj" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   { import = "astrocommunity.editing-support.text-case-nvim" },
+
   { import = "astrocommunity.editing-support.copilotchat-nvim" },
   {
-    {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      opts = {
-        model = "claude-3.7-sonnet",
-      },
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "claude-3.7-sonnet",
     },
   },
 
